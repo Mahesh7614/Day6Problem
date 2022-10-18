7aa5285164db2f5f036e16666418b09549b0be68
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-10-18 23:36:55 +0530

    Day6 Problems

---
+++ Day6Problem/FibonacciSeries.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6Problem
+{
+    public class FibonacciSeries
+    {
+        public int Number;
+
+        public FibonacciSeries(int number)
+        {
+            Number = number;
+        }
+        public void CalculateFibonacciSeries()
+        {
+            int num1 = 0,num2 = 1,num3;
+            Console.Write("Fibonacci Series is : ");
+            for (int i = 2; i <= Number; i++)
+            {
+                num3 = num1 + num2;
+                Console.Write( num3 + " ");
+                num1 = num2;
+                num2 = num3;                 
+            }
+            
+        }
+    }
+}
+// Fibonacci Series
---
+++ Day6Problem/ReverseNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6Problem
+{
+    public class ReverseNumber
+    {
+        public int RN;
+
+        public ReverseNumber(int rN)
+        {
+            RN = rN;
+        }
+        public void CalculateReverseNumber()
+        {
+            int reverse = 0, rem;
+
+            while (RN != 0)
+            {
+                rem = RN % 10;
+                reverse = (reverse * 10) + rem;
+                RN /= 10;
+            }
+            Console.WriteLine("Reverse Number is : " + reverse);
+        }
+
+    }
+}
+// Reverse Number
