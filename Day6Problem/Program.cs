07a95847e44ba8f0ade7339a8c607c6b81e5a098
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-10-19 15:08:56 +0530

    Day6 Logical Problems

--- Day6Problem/Program.cs
+++ Day6Problem/Program.cs
@@ -9,10 +9,10 @@ namespace Day6Problem
             UserInput Input = new UserInput();
             Input.fibonacciSeriesProblem(); // Calling Fibonacci Series Problem
             Input.perfectNumberProblem(); // Calling Perfect Number Problem
-            //Input.primeNumberProblem(); // Calling Prime Number Problem
-            //Input.reverseNumberProblem(); // calling Reverse Number Problen
-            //Input.couponNumberProblem(); // Calling Coupon Number Problem
-            //Input.stopWatchProblem(); //  calling StopWatch Program
+            Input.primeNumberProblem(); // Calling Prime Number Problem
+            Input.reverseNumberProblem(); // calling Reverse Number Problen
+            Input.couponNumberProblem(); // Calling Coupon Number Problem
+            Input.stopWatchProblem(); //  calling StopWatch Program
 
         }
     }
